fcab8272987ddb9317f9a31476340568defdb6e8
Thomas Colgan <btw928@frontend5.apocrita>
2019-02-20 13:10:59 +0000

    Rename WHATIDID.txt

--- 05_selection_analyses/extended_haplotype_homozygosity/preparing_input_files_to_EHH_methods.sh
+++ 05_selection_analyses/extended_haplotype_homozygosity/preparing_input_files_to_EHH_methods.sh
@@ -1,23 +1,16 @@
-#!/bin/env bash
-## Load modules
 module load vcftools
 module load plink
 module load R
 
-## Create directories for analysis:
 mkdir input
 mkdir results
-mkdir code
 
 cd input
-mkdir vcf
-mkdir hap
-mkdir map
-## Create a symbolic with original VCF:
-ln -s /data/autoScratch/weekly/btw928/2016-10-11_Bombus_population_genomics/results/2017-11-02_terrestris_combined_reanalysis/results/temp/09_final_clean/filtered_temp/02_sorted/2017-11-23-coverage_1/201$
+## Create a symbolic with original VCF: 
+ln -s /data/autoScratch/weekly/btw928/2016-10-11_Bombus_population_genomics/results/2017-11-02_terrestris_combined_reanalysis/results/temp/09_final_clean/filtered_temp/02_sorted/2017-11-23-coverage_1/2017-11-26-sample_n41_for_nsl/freebayes_hap0_minQ_1_minaltfrac_0.25_minCov1.Bter_n41.NC_only.minQ20.snps_only.no_hets.sites_low_freq.rare_variants_free.maxMeanDP100.recode.ann.vcf .
 
 ## Create a file containing list of chromosome/LG names:
-grep -v '#' freebayes_hap0_minQ_1_minaltfrac_0.25_minCov1.Bter_n41.NC_only.minQ20.snps_only.no_hets.sites_low_freq.rare_variants_free.maxMeanDP100.recode.ann.vcf | cut -f 1 | sort | uniq > chromosome_lis$
+grep -v '#' freebayes_hap0_minQ_1_minaltfrac_0.25_minCov1.Bter_n41.NC_only.minQ20.snps_only.no_hets.sites_low_freq.rare_variants_free.maxMeanDP100.recode.ann.vcf | cut -f 1 | sort | uniq > chromosome_list.txt
 
 ## Using the chromosome list, create individual vcf files for each chromosome:
 while read line
@@ -34,3 +27,24 @@ do
 abbrev_name="$(echo "$name" | cut -d '.' -f 1,2 - | cut -d '/' -f 2 )";
 Rscript VCF_to_genomatrix_SNPs.R "$name" ./hap/"$abbrev_name".gds ./hap/"$abbrev_name".hap.out;
 done
+
+## Generate a genetic map:
+## This approach works for nsl where genetic distance between snps is not required - only physical distance:
+for name in vcf/*vcf; do new_name="$(echo "$name" | cut -d '.' -f 1,2 | cut -d '/' -f 2 )";
+## Run plink
+plink --vcf "$name" \
+      --double-id \
+      --allow-extra-chr \
+      --out map/"$new_name";
+## Subset columns of interest:
+cut -f 1,2,3,4 map/"$new_name".bim > map/"$new_name".tmp;
+awk '{ print $1,$2,$4,$4 }' map/"$new_name".tmp | tr ' ' '\t' > map/"$new_name".map;
+done
+
+## Remove tmp files:
+rm -r tmp/
+rm -r vcf/
+
+## For running iHS, a genetic map containg genetic and physical distances between SNPs is required: 
+mkdir map_ihs
+ln -s ~/autoscratch_monthly_projects/2016-10-11_Bombus_population_genomics/results/2017-11-02_terrestris_combined_reanalysis/results/2017-11-19_run_ldhelmet/*post.txt .
