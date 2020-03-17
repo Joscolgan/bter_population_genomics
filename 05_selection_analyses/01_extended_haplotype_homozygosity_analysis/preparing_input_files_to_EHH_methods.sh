3f22a21c22b79678ca7ccb5f30f915bebd6909b1
Thomas Colgan <btw928@frontend5.apocrita>
2020-03-17 12:32:13 +0000

    Ensure up to date

--- 05_selection_analyses/01_extended_haplotype_homozygosity_analysis/preparing_input_files_to_EHH_methods.sh
+++ 05_selection_analyses/01_extended_haplotype_homozygosity_analysis/preparing_input_files_to_EHH_methods.sh
@@ -2,23 +2,26 @@ module load vcftools
 module load plink
 module load R
 
-mkdir input
+mkdir input/vcf
 mkdir results
 
 cd input
 ## Create a symbolic with original VCF: 
-ln -s /data/autoScratch/weekly/btw928/2016-10-11_Bombus_population_genomics/results/2017-11-02_terrestris_combined_reanalysis/results/temp/09_final_clean/filtered_temp/02_sorted/2017-11-23-coverage_1/2017-11-26-sample_n41_for_nsl/freebayes_hap0_minQ_1_minaltfrac_0.25_minCov1.Bter_n41.NC_only.minQ20.snps_only.no_hets.sites_low_freq.rare_variants_free.maxMeanDP100.recode.ann.vcf .
+#ln -s /data/autoScratch/weekly/btw928/2016-10-11_Bombus_population_genomics/results/2017-11-02_terrestris_combined_reanalysis/results/temp/09_final_clean/filtered_temp/02_sorted/2017-11-23-coverage_1/2017-11-26-sample_n41_for_nsl/freebayes_hap0_minQ_1_minaltfrac_0.25_minCov1.Bter_n41.NC_only.minQ20.snps_only.no_hets.sites_low_freq.rare_variants_free.maxMeanDP100.recode.ann.vcf .
+
+input="./input/filtered_n41/freebayes_hap0_minQ_1_minaltfrac_0.25_minCov1_n_41_snps_only_minQ20_maxmissing1_maxmeanDP100_NC_only_hets_removed_rare_removed.recode.vcf"
 
 ## Create a file containing list of chromosome/LG names:
-grep -v '#' freebayes_hap0_minQ_1_minaltfrac_0.25_minCov1.Bter_n41.NC_only.minQ20.snps_only.no_hets.sites_low_freq.rare_variants_free.maxMeanDP100.recode.ann.vcf | cut -f 1 | sort | uniq > chromosome_list.txt
+grep -v '#' "$input" | \
+cut -f 1 | sort | uniq > chromosome_list.txt
 
 ## Using the chromosome list, create individual vcf files for each chromosome:
 while read line
 do
-vcftools --vcf freebayes_hap0_minQ_1_minaltfrac_0.25_minCov1.Bter_n41.NC_only.minQ20.snps_only.no_hets.sites_low_freq.rare_variants_free.maxMeanDP100.recode.ann.vcf \
+vcftools --vcf "$input"  \
 --chr "$line" \
 --recode \
---out vcf/"$line"
+--out input/vcf/"$line"
 done < chromosome_list.txt
 
 ## The next step would be to create a haplotype file for each individual:
@@ -55,6 +58,25 @@ singularity exec /data/SBCS-Informatics/singularity/images/ubuntu_trusty-selscan
 --map ./input/map/"$new_name".map \
 --out results/"$new_name";
 done
+
+## Normalise output:
+singularity exec /data/SBCS-Informatics/singularity/images/ubuntu_trusty-selscan.img norm --ihs --files ./results/nsl/NC_0157*out
+
+for name in results/nsl/*norm; do new_name="$(echo "$name" | cut -d '/' -f 3 | cut -d '.' -f 1,2 )"; while read line; do echo "$new_name" >> results/nsl/"$new_name".tmp; done < "$name"; paste results/nsl/"$new_name".tmp "$name" | cut -f 1,3- > results/nsl/"$new_name".nsl.out.100bins.norm_renamed.txt; done
+
+rm -r results/nsl/*tmp
+
+cat results/nsl/NC_0157*norm_renamed.txt > results/nsl/combined_all_nsl_annotated_snps.txt
+
+cut -f 1,2 results/nsl/combined_all_nsl_annotated_snps.txt > results/annotated_nsl_snps/combined_all_nsl_annotated_snps_n46_snp_positions.txt
+
+vcftools --vcf ./input/filtered_n46/freebayes_hap0_minQ_1_minaltfrac_0.25_minCov1_n_46_snps_only_minQ20_maxmissing1_maxmeanDP100_NC_only_hets_removed_rare_removed.recode.vcf --positions ./results/annotated_nsl_snps/combined_all_nsl_annotated_snps_n46_snp_positions.txt --recode --out ./results/annotated_nsl_snps/combined_all_nsl_annotated_snps_n46_snp_positions
+
+java -Xmx4g -jar ~/src/2019_snpEff/snpEff/snpEff.jar Bter_1.0 results/annotated_nsl_snps/combined_all_nsl_annotated_snps_n46_snp_positions.recode.vcf > results/annotated_nsl_snps/combined_all_nsl_annotated_snps_n46_snp_positions.ann.vcf
+
+grep -v '#' results/annotated_nsl_snps/combined_all_nsl_annotated_snps_n46_snp_positions.ann.vcf | cut -f 8 | cut -d '|' -f 2,4,5 | paste <( grep -v '#' results/annotated_nsl_snps/combined_all_nsl_annotated_snps_n46_snp_positions.ann.vcf | cut -f 1,2) <(cut -f 7 ./results/nsl/combined_all_nsl_annotated_snps_n46.txt) - > results/annotated_nsl_snps/combined_all_nsl_annotated_snps_n46_snp_positions.annotated.txt
+
+sed -i 's/\t-/\t/g' results/annotated_nsl_snps/combined_all_nsl_annotated_snps_n46_snp_positions.annotated.txt
 
 ## For running iHS, a genetic map containg genetic and physical distances between SNPs is required: 
 mkdir map_ihs
